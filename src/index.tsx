76c97ab1686a0fa008260f724f1bcc0886e41885
Michael Spaxman <seanforyou23@gmail.com>
2018-11-06 08:59:49 -0500

    feat(a11y): let us keep our accessibility compliance in check

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+const axe = require('react-axe'); // tslint:disable-line
+if (process.env.NODE_ENV !== 'production') {
+  axe(React, ReactDOM, 1000);
+}
 
 import { App } from './app';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <Router><App/></Router>,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
