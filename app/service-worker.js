110ff6d6f4aef32cc25d3097bf3320ca121feed4
CircleCI <circleci@syndesis.io>
2019-05-23 16:37:45 +0000

    [skip ci] Updates

--- app/service-worker.js
+++ app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://syndesisio.github.io/syndesis-react/app/precache-manifest.c4a4d64ce6f4010dde74f6250534dace.js"
+  "https://syndesisio.github.io/syndesis-react/app/precache-manifest.b1274bed5dca6b780b7d204a7fdfee0d.js"
 );
 
 self.addEventListener('message', (event) => {
